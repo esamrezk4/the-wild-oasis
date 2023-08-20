4b435568a29e28b31e226bb8b77b2981ca45b952
esamrezk4 <92589088+esamrezk4@users.noreply.github.com>
2023-08-20 15:21:49 +0300

    setup supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://sbpazkchfdwkzujzuxvj.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicGF6a2NoZmR3a3p1anp1eHZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIzNjQ1NzUsImV4cCI6MjAwNzk0MDU3NX0.LYWfeBiok_FkEqng6mP-ultFElUzOWln-iJt6BkO-wg"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+export default supabase
